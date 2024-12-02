3c80c2fc2c7ab751bcdd33a6e7f9babde2393625
mbary <michal.barrington@gmail.com>
2024-12-02 07:27:44 +0100

    cleaned up the base class

--- Prometheus/device.py
+++ Prometheus/device.py
@@ -23,33 +23,24 @@ class HueResource:
     ## based on this, using inheritence, we can create subclasses such as lights, switches, zones, rooms etc.
     ## They will be extended by device-specific functionalities such as on/off and whatever the fuck else I manage to find
     def __init__(self, dev_dict:Dict, hue_hostname: str, hue_key: str) -> None:
-        self._parse_dev_dict(dev_dict=dev_dict)
-        self._hue_hostname = hue_hostname ## TODO remove hostaname, this should be kept in the bridge class
+        self._hue_hostname = hue_hostname 
         self._hue_key = hue_key
-        self.base_url = f"https://{self._hue_hostname}/clip/v2/resource/" ## TODO remove, this should be kept in the bridge class
-        self._headers = self._build_headers() ## TODO remove, this should be kept in the bridge class
-
+        self.base_url = f"https://{self._hue_hostname}/clip/v2/resource/"  
+        self._parse_dev_dict(dev_dict=dev_dict)
 
 
     def _parse_dev_dict(self, dev_dict: Dict) -> None:
         """Parses device data and creates general, device agnostic, attributes"""
-        self._dev_data = dev_dict["data"]
-        self.product_name = self._dev_data["product_data"]["product_name"]
+        self._dev_data = dev_dict
         self.resource_name = self._dev_data["metadata"]["name"]
-        self._services = {adict["rtype"]:adict["rid"] for adict in self._dev_data["services"]}
         self.id = dev_dict['id']
 
-    def _build_headers(self): ## TODO this should be changed to _build_body or something similar
-        headers = {           ##      because headers are common for all queries (thus in Bridgette class) while body will be unique to each request thus it should be on device level not manager level
-            'hue-application-key':self._hue_key,
-            'Content-Type':'application/json'
-        }
-        return headers
-    
+
     def _get(self, url: str) -> Dict:
         """Retrievies Device(s) info"""
         req = requests.get(url=url, headers=self._headers, verify=False)
         return json.loads(req.text)
+
     
     def _put(self, url: str, headers: Dict, body: Dict) -> None:
         """Modifies Device State"""
@@ -59,8 +50,8 @@ class HueResource:
 class HueLight(HueResource):
     ## TODO
     ## Implement light-specific functions such as on/off/brightness/colour etc.
-    def __init__(self, dev_dict: Dict, state: str) -> None:
-        super().__init__(dev_dict)
+    # def __init__(self, dev_dict: Dict, hue_hostname: str, hue_key: str) -> None:
+        # super().__init__(dev_dict, hue_hostname, hue_key)
 
     def _parse_dev_dict(self, dev_dict: Dict) -> None:
         super()._parse_dev_dict(dev_dict)    
